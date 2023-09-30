0f7f71e793d1ee6e96b7728fc4b464bb7e73c127
Kakackle <markowskimaciej98@gmail.com>
2023-09-30 11:35:32 +0200

    added whitenoise, env variables for deplopy, s3 storage

--- pegasus/settings.py
+++ pegasus/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -21,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tin=jv(d+%#gt5@d!%j+t2r*2n8&me$(8d5)l+p-y#(e%ed$w@'
+# SECRET_KEY = 'django-insecure-tin=jv(d+%#gt5@d!%j+t2r*2n8&me$(8d5)l+p-y#(e%ed$w@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '-tin=jv(d+%#gt5@d!%j+t2r*2n8&me$(8d5)l+p-y#(e%ed$w@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [" ", "127.0.0.1"]
 
 # Application definition
 
@@ -52,10 +53,12 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'drf_spectacular_sidecar',
     'django_extensions',
+    'storages',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
@@ -101,6 +104,9 @@ DATABASES = {
     }
 }
 
+# database_url='postgresql://postgres:v1q3gZdkQW3oyMEU@db.mloelszaaibsbbsztrir.supabase.co:5432/postgres'
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -146,9 +152,19 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = 'uploads'
 MEDIA_URL = '/media/'
+
+
+# S3
+
+AWS_S3_ACCESS_KEY_ID = os.environ.get("AWS_S3_ACCESS_KEY_ID")
+AWS_S3_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME='eu-north-1'
+AWS_QUERYSTRING_AUTH = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
