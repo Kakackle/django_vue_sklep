7132ecff6fabeef9f4891c2edcb717f03d2ba11f
Kakackle <markowskimaciej98@gmail.com>
2023-09-30 11:54:57 +0200

    forgotr to add default file storage as s3 in settings

--- pegasus/settings.py
+++ pegasus/settings.py
@@ -159,7 +159,7 @@ MEDIA_URL = '/media/'
 
 
 # S3
-
+DEFAULT_FILE_STORAGE = "storages.backends.s3.S3Storage"
 AWS_S3_ACCESS_KEY_ID = os.environ.get("AWS_S3_ACCESS_KEY_ID")
 AWS_S3_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
